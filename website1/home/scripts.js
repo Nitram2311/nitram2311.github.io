81032aff98f6b2defeecde01ca63477126a2fc3c
Nitram2311 <marschmitt23@gmail.com>
2026-07-20 21:38:28 +0200

    Delete website1 directory

--- website1/home/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("button").addEventListener("click", function() {
-    window.location.href = "../speisekarte/index.html";
-});
-
-document.getElementById("sbutton").addEventListener("click", function() {
-    window.location.href = "../Standort/index.html";
-});
