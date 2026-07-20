ff6dcbe8991df8b4e2040ecd48047d957f7b539e
Nitram2311 <marschmitt23@gmail.com>
2026-07-20 21:06:48 +0200

    Add files via upload

---
+++ website1/home/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", function() {
+    window.location.href = "../speisekarte/index.html";
+});
+
+document.getElementById("sbutton").addEventListener("click", function() {
+    window.location.href = "../Standort/index.html";
+});
